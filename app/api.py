30d04fc45dce20dbfff4bf62765f3c6bcbd133c6
ADRIAN GROPPER <agropper@healthurl.com>
2021-05-22 16:35:39 -0400

    added GETs

--- app/api.py
+++ app/api.py
@@ -1,9 +1,37 @@
 # app/api.py
 
 from fastapi import FastAPI
+from app.model import PostSchema
+posts = [
+    {
+        "id": 1,
+        "title": "Pancake",
+        "content": "Lorem Ipsum ..."
+    }
+]
+
+users = []
 
 app = FastAPI()
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your blog!."}
+
+@app.get("/posts", tags=["posts"])
+async def get_posts() -> dict:
+    return { "data": posts }
+
+
+@app.get("/posts/{id}", tags=["posts"])
+async def get_single_post(id: int) -> dict:
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": post
+            }
