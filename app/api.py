d3f0339b1cbd29676b1f2f3beee1b60e077d1af3
ADRIAN GROPPER <agropper@healthurl.com>
2021-05-23 12:11:25 -0400

    fixed jwt and decouple import errors

--- app/api.py
+++ app/api.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI, Body
 
 from app.model import PostSchema, UserSchema, UserLoginSchema
-# from app.auth.auth_handler import signJWT
+from app.auth.auth_handler import signJWT
 
 import couchdb
 
@@ -21,6 +21,12 @@ posts = [
 users = []
 
 app = FastAPI()
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
@@ -63,7 +69,15 @@ async def add_post(post: PostSchema) -> dict:
         "data": "post added."
     }
 
-# @app.post("/user/signup", tags=["user"])
-# async def create_user(user: UserSchema = Body(...)):
-#     users.append(user) # replace with db call, making sure to hash the password first
-#     return signJWT(user.email)
+@app.post("/user/signup", tags=["user"])
+async def create_user(user: UserSchema = Body(...)):
+     users.append(user) # replace with db call, making sure to hash the password first
+     return signJWT(user.email)
+
+@app.post("/user/login", tags=["user"])
+async def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
